1cad07c07d56f304d2215df1900c7d6d81f6fde3
Axel Sobkowiak <axel.sobkowiak2005@gmail.com>
2024-10-07 11:25:21 +0200

    login

--- logreg/login.php
+++ logreg/login.php
@@ -12,12 +12,18 @@
 		</div>
 		<form class="card-form">
 			<div class="input">
-				<input type="text" class="input-field" value="Alexander Parkinson" required/>
-				<label class="input-label">Login</label>
+				<input type="text" class="input-field" required/>
+				<label class="input-label" name ="login" id="login">Login</label>
 			</div>
+
+			<div class="input">
+				<input type="text" class="input-field" required/>
+				<label class="input-label" name ="email" id="login">Email</label>
+			</div>
+
 						<div class="input">
 				<input type="password" class="input-field" required/>
-				<label class="input-label">Mot de passe</label>
+				<label class="input-label" name="password" id="password">Mot de passe</label>
 			</div>
 			<div class="action">
 				<button class="action-button">Se connecter</button>
@@ -27,4 +33,43 @@
 			<p>En vous inscrivant, vous acceptez nos <a href="#">conditions générales</a></p>
 		</div> 
 	</div>
-</div></body>
+</div></body> 
+
+<?php
+
+session_start();
+include "../conn/dbConnect.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    try {
+       
+        $sql = "SELECT * FROM utilisateurs WHERE nom_utilisateur = :username";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+         
+            if (password_verify($password, $user['mot_de_passe'])) {
+                $_SESSION['username'] = $user['nom_utilisateur'];
+                $_SESSION['role'] = $user['role'];
+                echo "Vous êtes connecté en tant que " . $username;
+                header("Location: ../admin/consulter_tournois.php");
+                exit();
+            } else {
+                echo "Votre nom d'utilisateur ou votre mot de passe est incorrect";
+            }
+        } else {
+            echo "Votre nom d'utilisateur ou votre mot de passe est incorrect";
+        }
+    } catch (PDOException $e) {
+        echo "Erreur : " . $e->getMessage();
+    }
+}
+?>
+
