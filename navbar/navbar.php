44adb32136aa60e9f20d949a90d37a409d9e037b
Axel Sobkowiak <axel.sobkowiak2005@gmail.com>
2024-10-08 14:21:27 +0200

    Mise à jour de la nav bar

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -1,56 +1,51 @@
-
 <link href="tulipe/css/style.css" rel="stylesheet" />
-
 <link href="tulipe/css/bootstrap.css" rel="stylesheet" />
 
-  
-  
-  
-  <!-- header section strats -->
-  <header class="header_section">
+<!-- header section starts -->
+<header class="header_section">
     <div class="container-fluid">
-      <nav class="navbar navbar-expand-lg custom_nav-container">
-        <div class="" id="">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+            <div class="" id="">
 
-        <div class="custom_menu-btn">
-  <button onclick="openNav()">
-    <span class="s-1"> </span>
-    <span class="s-2"> </span>
-    <span class="s-3"> </span>
-  </button>
-  <div id="myNav" class="overlay">
-    <div class="overlay-content">
-      <a href="index.php">Accueil</a>
+                <div class="custom_menu-btn">
+                    <button onclick="openNav()">
+                        <span class="s-1"> </span>
+                        <span class="s-2"> </span>
+                        <span class="s-3"> </span>
+                    </button>
+                    <div id="myNav" class="overlay">
+                        <div class="overlay-content">
+                            <a href="index.php">Accueil</a>
 
-      <?php if (isset($_SESSION['username'])): ?>
-        <!-- Si l'utilisateur est connecté -->
-        <a href="#">Connecté(e) : <?php echo $_SESSION['username']; ?></a>
-        <a href="tulipe">Consulter les élèves</a>
-        <a href="reglog/logout.php">Déconnexion</a>
+                            <?php if (isset($_SESSION['username'])): ?>
+                                <!-- Si l'utilisateur est connecté -->
+                                <a href="#">Connecté(e) : <?php echo $_SESSION['username']; ?></a>
+                                
+                                <?php if ($_SESSION['role'] == 'Professeur'): ?>
+                                    <a href="/tulipe/crud/user/crudUser.php">Consulter les élèves</a>
+                                <?php endif; ?>
+                                
+                                <a href="reglog/logout.php">Déconnexion</a>
 
-        <?php if ($_SESSION['role'] == 'Professeur'): ?>
-          <a href="/tulipe/crud/user/crudUser.php">Consulter les élèves</a>
-        <?php endif; ?>
-      
-      <?php else: ?>
-        <!-- Si l'utilisateur n'est pas connecté -->
-        <a href="/tulipe/logreg/login.php">Connexion</a>
-      <?php endif; ?>
-      
+                            <?php else: ?>
+                                <!-- Si l'utilisateur n'est pas connecté -->
+                                <a href="/tulipe/logreg/login.php">Connexion</a>
+                            <?php endif; ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
-  </div>
-</div>
-</nav>
-</div>
 </header>
+<!-- end header section -->
 
-  <!-- end header section -->
-
-  <!-- jQery -->
-  <script src="/tulipe/js/jquery-3.4.1.min.js"></script>
-  <!-- bootstrap js -->
-  <script src="/tulipe/js/bootstrap.js"></script>
-  <!-- lightbox Gallery-->
-  <script src="/tulipe/js/ekko-lightbox.min.js"></script>
-  <!-- custom js -->
-  <script src="/tulipe/js/custom.js"></script>
+<!-- jQuery -->
+<script src="/tulipe/js/jquery-3.4.1.min.js"></script>
+<!-- bootstrap js -->
+<script src="/tulipe/js/bootstrap.js"></script>
+<!-- lightbox Gallery-->
+<script src="/tulipe/js/ekko-lightbox.min.js"></script>
+<!-- custom js -->
+<script src="/tulipe/js/custom.js"></script>
